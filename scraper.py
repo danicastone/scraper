5ad2713fb1afd9898479fec8ef04abbc46d1e537
danicastone <danicastone@gmail.com>
2024-01-17 20:20:55 +0000

    Cleaned up scrapped code and added comments

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ city = 'san_francisco-ca'
 
 url = 'https://fastpeoplesearch.com/name/' + first + '-' + last + '_' + city
 
+# try/catch loops here are probably overkill, but better safe than sorry
 try:
      subprocess.run('lynx -dump ' + url + ' > copy.txt', 
      shell=True, check=True)
@@ -26,24 +27,13 @@ with open('addresses.txt', 'r') as input:
      #read all the lines of the file into a list
      list = input.readlines()
      with open('database.csv', 'a') as output:
+     # write the first and last name on a new line at the bottom of the spreadsheet
+     # (with comma delimeters)
           output.write('\n' + first + ',' + last + ',')
+     # strip the line break from the end of the street address and write after the name
           output.writelines((list[0]).strip())
+     # replace the spaces in the city, state, and zip code with commas, and write to csv
           output.writelines(regex.sub('\s+', ',', list[1]))
-#     temp_line = list[1]
- #    print(list)
- #    print(list[1])
- #    print(temp_line)
-#     with open ('temp.txt', 'w') as temp:
-#          temp.writelines(temp_line)
-#          regex.sub('\s+', ',', temp.txt)
-#          print(temp_line) 
-#          list[1] = temp.readlines()
-#          print(list[1])
-#          with open ('database.csv', 'a') as output:
-#               output.writelines(list[0])
-#               output.writelines(list[1])
-#               i.replace('/n/[ ]*|[ ]+/g', ',')
-#               output.writelines(i)
-
-input.close()
-output.close()   
+     # close the files back up, we like to keep things tidy 
+     output.close()
+input.close()   
